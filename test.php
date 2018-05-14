db79a174955bd55e293bc84f728378abf52d2dfc
shaopeng <yanyujie0706@sina.com>
2018-05-14 22:48:59 +0800

    first

---
+++ test.php
@@ -0,0 +1,43 @@
+<?php
+include_once __DIR__.'/changeDefine.php';
+include_once APP_PATH.'/vendor/autoload.php';
+
+use Pheanstalk\Pheanstalk;
+/**
+ * Created by PhpStorm.
+ * User: Yujie
+ * Date: 2018/5/12
+ * Time: 02:58
+ */
+$beanstalk = new Pheanstalk('127.0.0.1');
+
+$beanstalk->useTube('foo');
+//$beanstalk->putInTube('foo','say hello');
+//echo "--------3--------\n";
+$ret = $beanstalk->put('say hello world');
+echo "{$ret}\n";
+echo "---------4-------\n";
+$ret = $beanstalk->statsTube('foo');
+exit(json_encode($ret)."\n");
+//$job = $beanstalk->watch('foo')->reserve(0);
+//echo (json_encode($job)."\n");
+
+//$ret = $beanstalk->peekReady('foo');
+$job = $beanstalk->reserveFromTube('foo');
+
+//$job = $beanstalk->reserve();
+//$job = $beanstalk->reserveFromTube('foo');
+//exit(json_encode($job)."\n");
+
+//while($job = $beanstalk->reserveFromTube('foo')){
+//    exit(json_encode($job)."\n");
+//}
+
+echo "---------5-------\n";
+$aa = $job->getData();
+echo "---------6-------\n";
+print_r($aa);
+
+echo "\n";
+
+$beanstalk->delete($job);
