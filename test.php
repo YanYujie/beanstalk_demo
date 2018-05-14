d8fe7757291b4d3f30d969a293a69457ef2b4d5c
shaopeng <yanyujie0706@sina.com>
2018-05-14 23:08:16 +0800

    first

--- test.php
+++ test.php
@@ -11,42 +11,42 @@ use Pheanstalk\Pheanstalk;
  */
 $beanstalk = new Pheanstalk('127.0.0.1');
 
-//$beanstalk->useTube('foo');
-////$beanstalk->putInTube('foo','say hello');
-////echo "--------3--------\n";
-//$ret = $beanstalk->put('say hello world');
-//echo "{$ret}\n";
+$beanstalk->useTube('foo');
+//$beanstalk->putInTube('foo','say hello');
+//echo "--------3--------\n";
+$ret = $beanstalk->put('say hello world');
+echo "{$ret}\n";
 //echo "---------4-------\n";
-$ret = $beanstalk->statsTube('foo');
-echo (json_encode($ret)."\n");
-
-$job = $beanstalk->watch('foo');
-echo "---------4-------\n";
-echo (json_encode($job)."\n");
-$job = $beanstalk->reserve();
-//$job->reserve();
-echo "---------4.5-------\n";
-echo (json_encode($job)."\n");
-
-//$ret = $beanstalk->peekReady('foo');
-//$job = $beanstalk->reserveFromTube('foo');
-
+//$ret = $beanstalk->statsTube('foo');
+//echo (json_encode($ret)."\n");
+//
+//$job = $beanstalk->watch('foo');
+//echo "---------4-------\n";
+//echo (json_encode($job)."\n");
 //$job = $beanstalk->reserve();
-//$job = $beanstalk->reserveFromTube('foo');
-//exit(json_encode($job)."\n");
-
-//while($job = $beanstalk->reserveFromTube('foo')){
-//    exit(json_encode($job)."\n");
-//}
-
-echo "---------5-------\n";
-$aa = $job->getData();
-echo "---------6-------\n";
-print_r($aa);
-
-echo "\n";
-
-$beanstalk->delete($job);
-
-$ret = $beanstalk->statsTube('foo');
-echo json_encode($ret)."\n";
+////$job->reserve();
+//echo "---------4.5-------\n";
+//echo (json_encode($job)."\n");
+//
+////$ret = $beanstalk->peekReady('foo');
+////$job = $beanstalk->reserveFromTube('foo');
+//
+////$job = $beanstalk->reserve();
+////$job = $beanstalk->reserveFromTube('foo');
+////exit(json_encode($job)."\n");
+//
+////while($job = $beanstalk->reserveFromTube('foo')){
+////    exit(json_encode($job)."\n");
+////}
+//
+//echo "---------5-------\n";
+//$aa = $job->getData();
+//echo "---------6-------\n";
+//print_r($aa);
+//
+//echo "\n";
+//
+//$beanstalk->delete($job);
+//
+//$ret = $beanstalk->statsTube('foo');
+//echo json_encode($ret)."\n";
