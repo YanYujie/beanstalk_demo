4e95c291ae77dbe198dc2c638e7d335270d856b9
shaopeng <yanyujie0706@sina.com>
2018-05-14 23:55:15 +0800

    first

--- worker.php
+++ worker.php
@@ -7,7 +7,7 @@
  */
 include_once __DIR__.'/changeDefine.php';
 include_once APP_PATH.'/vendor/autoload.php';
-
+use Pheanstalk\Exception;
 use Pheanstalk\Pheanstalk;
 echo "--------------argc---------------\n";
 print_r($argc);
@@ -18,15 +18,16 @@ echo "\n";
 
 $beanstalk = new Pheanstalk('127.0.0.1');
 
-$beanstalk->putInTube('foo','say hello worker');
+//$beanstalk->putInTube('foo','say hello worker');
 echo "---------4-------\n";
-//try {
-//    $ret = $beanstalk->statsTube('foo');
-//}catch(PharException $e){
-//    print_r($e);
-//    echo "\n";
-//}
-//echo (json_encode($ret)."\n");
+
+try {
+    $ret = $beanstalk->statsTube('foo');
+}catch(Exception $e){
+    print_r($e);
+    echo "\n";
+}
+echo (json_encode($ret)."\n");
 
 
 //$ret = $beanstalk->peekReady('foo');
