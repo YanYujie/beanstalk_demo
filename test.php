f2025a55a622ff8977757a1f30fc71687d10e187
shaopeng <yanyujie0706@sina.com>
2018-05-15 15:04:52 +0800

    first

--- test.php
+++ test.php
@@ -2,13 +2,18 @@
 include_once __DIR__.'/changeDefine.php';
 include_once APP_PATH.'/vendor/PheanstalkClient.php';
 
-echo "--------------argc---------------\n";
-print_r($argc);
-echo"\n";
-echo "------------argv-----------------\n";
-print_r($argv);
-echo "\n";
+//echo "--------------argc---------------\n";
+//print_r($argc);
+//echo"\n";
+//echo "------------argv-----------------\n";
+//print_r($argv);
+//echo "\n";
+//
+//$pheanstalk = PheanstalkClient::getInstance();
+//$ret = $pheanstalk->listTubes();
 
-$pheanstalk = PheanstalkClient::getInstance();
-$ret = $pheanstalk->listTubes();
+$pheanstalk = new \Pheanstalk\Pheanstalk('127.0.0.1');
+$ret = $pheanstalk->statsTube('foo');
+
+
 print_r($ret);echo "\n";
