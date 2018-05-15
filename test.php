e5284777a90beb7dc2b08f247b2f22614938fde0
shaopeng <yanyujie0706@sina.com>
2018-05-15 14:50:52 +0800

    first

--- test.php
+++ test.php
@@ -2,6 +2,13 @@
 include_once __DIR__.'/changeDefine.php';
 include_once APP_PATH.'/vendor/PheanstalkClient.php';
 
+echo "--------------argc---------------\n";
+print_r($argc);
+echo"\n";
+echo "------------argv-----------------\n";
+print_r($argv);
+echo "\n";
+
 $pheanstalk = PheanstalkClient::getInstance();
 $ret = $pheanstalk->listTubes();
 print_r($ret);echo "\n";
