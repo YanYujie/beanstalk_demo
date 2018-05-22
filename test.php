6c5c8112e463c2081a6199cda01f6564e8ab9a58
shaopeng <yanyujie0706@sina.com>
2018-05-22 12:02:41 +0800

    first

--- test.php
+++ test.php
@@ -3,19 +3,18 @@ include_once __DIR__.'/changeDefine.php';
 include_once APP_PATH.'/vendor/PheanstalkClient.php';
 
 
-$pheanstalk = new \Pheanstalk\Pheanstalk('127.0.0.1');
-try{
-    $ret = $pheanstalk->statsTube('/usr/bin/php');
-    echo "----------------before---------------\n";
-    print_r($ret);echo "\n";
-}catch(\Pheanstalk\Exception $e){
-    echo "-----------------------------\n";
-    print_r($e->getMessage());
-    echo "\n";
-}
-//
-//$job = $pheanstalk->watch('/usr/bin/php')->reserve();
-//echo "------------------later------------------\n";
-//$ret = $pheanstalk->statsTube('/usr/local/php/bin/php');
-//print_r($ret);echo "\n";
+//$pheanstalk = new \Pheanstalk\Pheanstalk('127.0.0.1');
+//try{
+//    $ret = $pheanstalk->statsTube('/usr/bin/php');
+//    echo "----------------before---------------\n";
+//    print_r($ret);echo "\n";
+//}catch(\Pheanstalk\Exception $e){
+//    echo "-----------------------------\n";
+//    print_r($e->getMessage());
+//    echo "\n";
+//}
 
+
+$pheanstalk = PheanstalkClient::getInstance();
+$ret = $pheanstalk->statsTube('/usr/bin/php');
+print_r($ret);echo "\n";
