836b71998c50c28b69ea948a3e1cfda2415c2d93
shaopeng <yanyujie0706@sina.com>
2018-05-22 11:49:47 +0800

    first

--- test.php
+++ test.php
@@ -4,8 +4,12 @@ include_once APP_PATH.'/vendor/PheanstalkClient.php';
 
 
 $pheanstalk = new \Pheanstalk\Pheanstalk('127.0.0.1');
-
-$ret = $pheanstalk->statsTube('/usr/bin/php');
+try{
+    $ret = $pheanstalk->statsTube('/usr/bin/php');
+}catch(\Pheanstalk\Exception $e){
+    echo "-----------------------------\n";
+    print_r($e->getMessage());
+}
 echo "----------------before---------------\n";
 print_r($ret);echo "\n";
 $job = $pheanstalk->watch('/usr/bin/php')->reserve();
